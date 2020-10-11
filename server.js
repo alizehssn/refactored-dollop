05e31364d83a5e5c5fa3ab26b02f0dcf55a33b6f
Botticelli <alizehssn@gmail.com>
2020-10-11 17:43:09 -0500

    Finished Schema and inital functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import our dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 // Create/configure our MySQL connection
 const connection = mysql.createConnection({
@@ -20,8 +21,7 @@ connection.connect(err => {
     mainPrompt();
 });
 
-// Prompt 
-
+// Main Prompts for User Actions
 function mainPrompt() {
     inquirer
         .prompt({
@@ -29,13 +29,15 @@ function mainPrompt() {
             type: "list",
             message: "What would you like to do?",
             choices: [
-                "Add Department",
-                "Add Roles",
-                "Add Employees",
-                "View Departments",
-                "View Roles",
-                "View Employees",
-                "Update Employees",
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employee's By Role",
+                "Add New a Employee",
+                "Add Employee Department",
+                "Add Employee Role",
+                "Add an Employee's Department",
+                "Update Employee's Role",
+                "Update Employee's Role",
                 "Exit"
             ]
         }).then(mainPromptResponse);
@@ -43,6 +45,60 @@ function mainPrompt() {
 
 function mainPromptResponse({ action }) {
     switch (action) {
-
+        case "View All Employees":
+            viewAllEmployees();
+            break;
+        case "View All Employees By Department":
+            viewbyDept();
+            break;
+        case "View All Employee's By Role":
+            viewbyRole();
+            break;
+        case "Add New a Employee":
+            addEmployee();
+            break;
+        case "Add Employee Department":
+            addDept();
+            break;
+        case "Add Employee Role":
+            addEmployeeRole();
+            break;
+        case "Add an Employee's Department":
+            addEmployeeDept();
+            break;
+        case "Update Employee's Role":
+            updateRole();
+            break;
+        case "Exit":
+        default:
+            console.log("End Session");
+            connection.end();
     }
 }
+
+
+//Prompt Response Functions:
+
+//View Functions:
+function viewAllEmployees() {
+    connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) {
+            throw err;
+        }
+        console.log(res);
+        connection.end();
+    })
+
+};
+
+function viewbyDept() {
+
+
+}
+
+
+
+//Add Functions:
+
+
+//Update Functions
